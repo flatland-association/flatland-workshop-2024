f9111653976cbf738e7858a4bc07f51eb7824e10
Thomas Dubach <thomas.dubach@ivt.baug.ethz.ch>
2024-11-19 16:28:49 +0100

    start implementation of simple rail environment

--- next-flatland/gen_env.py
+++ next-flatland/gen_env.py
@@ -2,18 +2,9 @@ from abc import abstractmethod, ABCMeta
 from collections import defaultdict
 from typing import Set, Generic, TypeVar, Any, List, Dict
 
-import numpy as np
-from examples.custom_observation_example_02_SingleAgentNavigationObs import SingleAgentNavigationObs
-from flatland.core.grid.grid4_utils import get_new_position
-from flatland.envs.line_generators import sparse_line_generator
-from flatland.envs.rail_env import RailEnv
-from flatland.envs.rail_env_action import RailEnvActions
-from flatland.envs.rail_generators import sparse_rail_generator
-from flatland.utils.rendertools import RenderTool
-
 
 # TODO type hints and generics domain-agnostic
-class Entity(metaclass=ABCMeta):
+class Agent(metaclass=ABCMeta):
     @property
     @abstractmethod
     def state(self):
@@ -26,8 +17,8 @@ class Entity(metaclass=ABCMeta):
     @abstractmethod
     def rules(self) -> Set[Any]:
         """
-        A set of internal rules to determine if a proposed effect with the current entity is allowed
-or not.
+                A set of internal rules to determine if a proposed effect with the current entity is allowed
+        or not.
         """
         raise NotImplementedError()
 
@@ -40,21 +31,22 @@ or not.
         raise NotImplementedError()
 
 
-class Resource(Entity):
+class Resource(Agent):
     """
     Resource to be allocated. Agents occupy or "use" the resources to achieve certain
     action. e.g. track of train system, bandwidth of internet, storage space of warehouse systems.
     Except the 3 basic attributes, a resource can also have other attributes like capacity ,
     location , etc.
     """
+
     pass
 
 
-class Agent(Entity):
+class Agent(Agent):
     """
-    Agents drive the internal dynamics of the system. An agent has a policy , which is a
-mapping from current state (including agent, object, and resource's objectives) to an action.
-Agent is the only entity that can propose an action to modify the system.
+        Agents drive the internal dynamics of the system. An agent has a policy , which is a
+    mapping from current state (including agent, object, and resource's objectives) to an action.
+    Agent is the only entity that can propose an action to modify the system.
     """
 
     @property
@@ -68,15 +60,15 @@ Agent is the only entity that can propose an action to modify the system.
     @abstractmethod
     def act(self):
         """
-        An agent takes its current observable environment into account and proposes an action that tries
-to trigger an effect based on its policy .
-Agents' actions are the internal driver of state evolution. Or in plain words, agents act to change the
-system in order to achieve its goal.
+                An agent takes its current observable environment into account and proposes an action that tries
+        to trigger an effect based on its policy .
+        Agents' actions are the internal driver of state evolution. Or in plain words, agents act to change the
+        system in order to achieve its goal.
         """
         raise NotImplementedError()
 
 
-EntityType = TypeVar("EntityType", bound=Entity)
+EntityType = TypeVar("EntityType", bound=Agent)
 StateType = TypeVar("StateType")
 
 
@@ -86,17 +78,21 @@ class Relation(Generic[EntityType, StateType]):
     A Relation also attaches to a set of rule s that need to be considered while proposing an effect involving the relation.
     """
 
-    def __init__(self, fromEntity: EntityType, toEntity: EntityType, ):
-        self._fromEntity = fromEntity
-        self._toEntity = toEntity
+    def __init__(
+        self,
+        from_entity: EntityType,
+        to_entity: EntityType,
+    ):
+        self._from_entity = from_entity
+        self._to_entity = to_entity
 
     @property
-    def fromEntity(self) -> EntityType:
-        return self._fromEntity
+    def from_entity(self) -> EntityType:
+        return self._from_entity
 
     @property
-    def toEntity(self) -> EntityType:
-        return self._toEntity
+    def to_entity(self) -> EntityType:
+        return self._to_entity
 
 
 RelationType = TypeVar("RelationType", bound=Relation)
@@ -119,17 +115,17 @@ class SystemSnapshot(Generic[EntityType, RelationType]):
         return self.relations
 
 
-class Effect():
+class Effect:
     """
-    An effect is a proposed modification of the current state. It can only do the following:
-Change internal state of entities
-Add/remove relations
-Add/remove entities
-For example, a movement of a train is an effect that proposes the following changes
-Remove the relation between the current track and the train
-Add a new relation to the new track it wants to travel to
-A breakdown of a train or an agent is equivalent to Change internal state from operating to
-malfunction
+        An effect is a proposed modification of the current state. It can only do the following:
+    Change internal state of entities
+    Add/remove relations
+    Add/remove entities
+    For example, a movement of a train is an effect that proposes the following changes
+    Remove the relation between the current track and the train
+    Add a new relation to the new track it wants to travel to
+    A breakdown of a train or an agent is equivalent to Change internal state from operating to
+    malfunction
     """
 
     pass
@@ -138,18 +134,18 @@ malfunction
 EffectsType = TypeVar("EffectsType", bound=Effect)
 
 
-class Arbiter():
+class Arbiter:
     """
-    Arbiter takes a proposed effect and the set of rules involved for the proposal, decide whether
-to accept or reject the effect .
-An arbiter works in 2 levels
-Local : checks the rules regarding the change of an entity's internal state without considering
-the outside world
-Global : checks the rules regarding the relation between entities.
-Arbiter can also add new effect to the proposed effects to complete the state change. For
-example, a train proposes to move from one track to another. If these two tracks involves a turn, the
-arbiter adds another effect to change the train's internal state "direction" to the direction of the
-new track.
+        Arbiter takes a proposed effect and the set of rules involved for the proposal, decide whether
+    to accept or reject the effect .
+    An arbiter works in 2 levels
+    Local : checks the rules regarding the change of an entity's internal state without considering
+    the outside world
+    Global : checks the rules regarding the relation between entities.
+    Arbiter can also add new effect to the proposed effects to complete the state change. For
+    example, a train proposes to move from one track to another. If these two tracks involves a turn, the
+    arbiter adds another effect to change the train's internal state "direction" to the direction of the
+    new track.
     """
 
     @abstractmethod
@@ -157,11 +153,11 @@ new track.
         raise NotImplementedError()
 
 
-class Propagator():
+class Propagator:
     """
-    Given the valid effects , a propagator updates the system state (entities and relations). Note that if
-multiple effects are propagated at the same time, the propagator needs to handle potential conflicts
-between the effects.
+        Given the valid effects , a propagator updates the system state (entities and relations). Note that if
+    multiple effects are propagated at the same time, the propagator needs to handle potential conflicts
+    between the effects.
     """
 
     @abstractmethod
@@ -176,17 +172,13 @@ class SystemState:
         raise NotImplementedError()
 
     @abstractmethod
-    def pullActions(self):
+    def pull_actions(self):
         raise NotImplementedError()
 
 
-class GenEnvSimulation():
+class GenEnvSimulation:
 
-    def __init__(self,
-                 propagator: Propagator,
-                 state: SystemState,
-                 arbiter: Arbiter
-                 ):
+    def __init__(self, propagator: Propagator, state: SystemState, arbiter: Arbiter):
         self.propagator = propagator
         self.state = state
         self.arbiter = arbiter
@@ -198,170 +190,13 @@ class GenEnvSimulation():
     def run(self):
         dones = self.step()
 
-        while (not (all(dones.values()))):
+        while not (all(dones.values())):
             self.queue.clear()
             dones = self.step()
 
     def step(self):
-        self.addEffects(self.state.actionsToEffects(self.state.pullActions()))
+        self.addEffects(self.state.actionsToEffects(self.state.pull_actions()))
         # TODO malfuntion/env
         self.queue = self.arbiter.check_rules(self.queue)
         dones = self.propagator.propagate(self.queue)
         return dones
-
-
-class RailEnvSystemStateWrapper(SystemState):
-    def __init__(self, env: RailEnv):
-        self.env = env
-
-    def pullActions(self):
-        action_dict: Dict[int, RailEnvActions] = {}
-        for a in range(self.env.get_num_agents()):
-            action = np.random.randint(0, 5)
-            action_dict[a] = action
-        return action_dict
-
-    def actionsToEffects(self, action_dict):
-        return [action_dict]
-
-
-class RailEnvPropagatorWrapper(Propagator):
-    def __init__(self, env: RailEnv, render_mode="rgb_array"):
-        self.env = env
-        self.render_mode = render_mode
-        self.env_renderer = RenderTool(env)
-
-    def propagate(self, action_dict):
-        obs, rews, dones, infos = self.env.step(action_dict[0])
-        if self.render_mode is not None:
-            # We render the initial step and show the obsered cells as colored boxes
-            self.env_renderer.render_env(show=True, frames=True, show_observations=True, show_predictions=False)
-        print(dones)
-        return dones
-
-
-class RailEnvArbiterWrapper(Arbiter):
-    def __init__(self, env: RailEnv):
-        self.env = env
-
-    def check_rules(self, effects):
-        return effects
-
-
-class RailPropagator(Propagator):
-    pass
-
-
-# TODO malfunctions
-# TODO agent speeds, timetables etc.
-# TODO stations?
-# TODO lines?
-class RailState(SystemState):
-
-    # TODO super call -> generalise resources, objects, relations....
-    def __init__(self, cells, transitions, num_agents, agent_minimum_running_time):
-        # vertices
-        self.cells = cells
-
-        # resource-resource relations
-        self.transitions = transitions
-
-        # agent-resource relations
-        self.agent_cell_relations = {}  # agent -> cell
-
-        # agent internal states
-        self.num_agents = num_agents
-        self.agent_cell_ticks = {}  # agent -> Optional[int]
-        self.agent_minimum_running_time = agent_minimum_running_time  # agent -> int
-
-    @staticmethod
-    def fromRailEnv(env: RailEnv) -> "RailState":
-        trans = defaultdict(lambda: [])
-        cells = set()
-        for row in range(env.height):
-            for col in range(env.width):
-                cell_connected = False
-                for direction in range(4):
-                    transitions = env.rail.get_transitions(row, col, direction)
-                    for neigh_direction, is_valid in enumerate(transitions):
-                        if is_valid:
-                            neighbor = get_new_position((row, col), neigh_direction)
-                            cell_connected = True
-                            trans[(row, col, direction)].append((*neighbor, neigh_direction))
-                    if cell_connected:
-                        cells.add((row, col, direction))
-        return RailState(cells, trans, env.get_num_agents(), {i: env.agents[i].speed_counter.max_count for i in range(env.get_num_agents())})
-
-    def actionsToEffects(self, action_dict):
-        # TODO update positions and ticks, reset ticks to 0 when moving to next cell, no ticks = not entered, ticks but no position = exited, ticks + position = in grid/active
-        raise NotImplementedError()
-
-    def pullActions(self):
-        # TODO random policy for all active agents
-        # TODO generalize agent policy
-        raise NotImplementedError()
-
-
-class RailArbiter(Arbiter):
-    pass
-
-
-class RailEnvSimulationVerifier(GenEnvSimulation):
-    def __init__(self,
-                 propagator: RailPropagator,
-                 state: RailState,
-                 arbiter: RailArbiter,
-                 env: RailEnv
-                 ):
-        super().__init__(propagator, state, arbiter)
-        self.env = env
-
-    def checkSameState(self, env: RailEnv, state: RailState):
-        raise NotImplementedError()
-
-    def step(self):
-        dones = super().step()
-        self.env.step(self.queue[0])
-        self.checkSameState(self.env, self.state)
-        return dones
-
-
-if __name__ == '__main__':
-    nAgents = 2
-    n_cities = 2
-    max_rails_between_cities = 2
-    max_rails_in_city = 4
-    seed = 0
-    env = RailEnv(
-        width=30,
-        height=40,
-        rail_generator=sparse_rail_generator(
-            max_num_cities=n_cities,
-            seed=seed,
-            grid_mode=True,
-            max_rails_between_cities=max_rails_between_cities,
-            max_rail_pairs_in_city=max_rails_in_city
-        ),
-        line_generator=sparse_line_generator(),
-        number_of_agents=nAgents,
-        obs_builder_object=SingleAgentNavigationObs()
-    )
-
-    if True:
-        simulation = GenEnvSimulation(
-            propagator=RailEnvPropagatorWrapper(env),
-            state=RailEnvSystemStateWrapper(env),
-            arbiter=RailEnvArbiterWrapper(env),
-        )
-        env.reset()
-        simulation.run()
-
-    if True:
-        env.reset()
-        simulation = RailEnvSimulationVerifier(
-            propagator=RailPropagator(),
-            state=RailState.fromRailEnv(env),
-            arbiter=RailArbiter(),
-            env=env
-        )
-        simulation.run()
---
+++ next-flatland/rail_prototyp.py
@@ -0,0 +1,96 @@
+from dataclasses import dataclass
+from gen_env import Agent, Arbiter, Effect, Agent, Relation, Resource, SystemState
+
+
+@dataclass()
+class RailResource(Resource):
+    id: int
+    valid_routes: dict[Resource, list[Resource]]
+
+
+@dataclass()
+class TrainAgent(Agent):
+    id: int
+    previous_position: Resource
+
+    def act(self):
+        return AddAndRemoveRelation(
+            remove_relation=Relation(
+                self, self.previous_position, self.current_position
+            ),
+            add_relation=Relation(self, self.current_position, self.next_position),
+        )
+
+
+@dataclass()
+class AddAndRemoveRelation(Effect):
+    add_relation: Relation
+    remove_relation: Relation
+
+
+@dataclass()
+class AddRelation(Effect):
+    relation: Relation
+
+
+@dataclass()
+class RemoveRelation(Effect):
+    relation: Relation
+
+
+class RailState(SystemState):
+    def __init__(
+        self,
+        agents: list[Agent],
+        relations: list[Relation],
+        resources: list[RailResource],
+    ):
+        self.entities = agents
+        self.relations = relations
+        self.resources = resources
+
+    def pull_actions(self):
+        for agent in self.entities:
+            if isinstance(agent, TrainAgent):
+                yield agent.act()
+
+    def add_entity(self, entity: Agent):
+        self.entities.append(entity)
+
+    def add_relation(self, from_entity: Agent, to_entity: Agent):
+        self.relations.append(Relation(from_entity, to_entity))
+
+    def remove_relation(self, from_entity: Agent, to_entity: Agent):
+        self.relations.remove(Relation(from_entity, to_entity))
+
+    def add_resource(self, resource: RailResource):
+        self.resources.append(resource)
+
+
+class RailArbiter(Arbiter):
+    def check_rules(self, effects: list[Effect]) -> list[Effect]:
+        valid_effects: list[Effect] = []
+        for effect in effects:
+            if isinstance(effect, AddAndRemoveRelation):
+                agent = effect.add_relation.from_entity
+                resource_to_add = effect.add_relation.to_entity
+                resource_to_remove = effect.remove_relation.to_entity
+
+                # occupation check
+                valid_occupation = resource_to_add
+
+                # transition check
+                valid_transition = (
+                    resource_to_add.id
+                    in resource_to_remove.valid_routes[agent.previous_position]
+                )
+                if valid_transition:
+                    valid_effects.extend(
+                        [
+                            AddRelation(effect.add_relation),
+                            RemoveRelation(effect.remove_relation),
+                        ]
+                    )
+                print("agent {} stopped at invalid transition".format(agent.id))
+
+        return valid_effects

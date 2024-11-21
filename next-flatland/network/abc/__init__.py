76facc2bab31d4d0185b709b49e5db8cee5ed6dc
flfuchs <flfuchs@student.ethz.ch>
2024-11-21 10:42:08 +0100

    [Network] Add ABC forom OPB

---
+++ next-flatland/network/abc/__init__.py
@@ -0,0 +1,4 @@
+from .immutablenetwork_abc import ImmutableNetworkABC
+from .link import LinkABC, EndNodeIdPair
+from .mutablenetwork import MutableNetworkABC, LinkIndex
+from .node import NodeABC, NodeId, NodeIndex
